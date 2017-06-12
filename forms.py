02fa74b3ec9f7dfb7cf802d866878de5447ed532
Packetdancer <packetdancer@gmail.com>
2017-06-11 17:15:59 -0700

    Improve size of post/reply widgets.

--- forms.py
+++ forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class PostForm(forms.Form):
-    subject = forms.CharField(label='Subject', max_length=60)
-    text = forms.CharField(label='Post', widget=forms.Textarea, max_length=6000)
+    subject = forms.CharField(label='Subject', widget=forms.Textarea(attrs={'rows': 1, 'cols': 80}), max_length=60)
+    text = forms.CharField(label='Post', widget=forms.Textarea(attrs={'rows': 10, 'cols': 80}), max_length=6000)
 
 
 class ReplyForm(forms.Form):
-    text = forms.CharField(label='Reply', widget=forms.Textarea, max_length=6000)
+    text = forms.CharField(label='', widget=forms.Textarea(attrs={'rows': 10, 'cols': 80}), max_length=6000)
