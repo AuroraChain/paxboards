97312229db51ffd211df4b639091a482e830f838
Packetdancer <packetdancer@gmail.com>
2017-06-12 22:49:35 -0700

    Correct bug in URL registrations.

--- urls.py
+++ urls.py
@@ -5,7 +5,6 @@ from paxboards.views import show_boardlist, show_board, show_thread, submit_post
 
 urlpatterns = [
     url(r'^$', show_boardlist, name="boardlist"),
-    url(r'^all/$', show_boardlist, name="boardlist"),
     url(r'^(?P<board_id>\d+)/$', show_board, name="board"),
     url(r'^(?P<board_id>\d+)/(?P<post_id>\d+)/$', show_thread, name="thread"),
     url(r'^(?P<board_id>\d+)/post/$', submit_post, name="post"),
